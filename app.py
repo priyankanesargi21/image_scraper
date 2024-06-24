05be28ff9c66e8b7cf0919ac60ea2d6b5c7da5b0
priyankanesargi21 <priyankanesargi@gmail.com>
2024-06-24 10:55:11 +0530

    image scraper

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
+from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
 #from urllib.request import urlopen as uReq
@@ -22,30 +21,24 @@ def index():
 
                     # query to search for images
                     query = request.form['content'].replace(" ","")
-
-                            # directory to store downloaded images
+                    # directory to store downloaded images
                     save_directory = "images/"
-
-                            # create the directory if it doesn't exist
+                    search_url = f"https://www.google.com/search?q={query}&sxsrf=AJOqlzUuff1RXi2mm8I_OqOwT9VjfIDL7w:1676996143273&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiq-qK7gaf9AhXUgVYBHYReAfYQ_AUoA3oECAEQBQ&biw=1920&bih=937&dpr=1#imgrc=1th7VhSesfMJ4M"
+                    # create the directory if it doesn't exist
                     if not os.path.exists(save_directory):
                         os.makedirs(save_directory)
 
+                    # fetch the search results page
+                    response = requests.get(search_url)
 
 
-                            # fake user agent to avoid getting blocked by Google
-                    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
-
-                            # fetch the search results page
-                    response = requests.get(f"https://www.google.com/search?q={query}&sxsrf=AJOqlzUuff1RXi2mm8I_OqOwT9VjfIDL7w:1676996143273&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiq-qK7gaf9AhXUgVYBHYReAfYQ_AUoA3oECAEQBQ&biw=1920&bih=937&dpr=1#imgrc=1th7VhSesfMJ4M")
-
-
-                            # parse the HTML using BeautifulSoup
+                    # parse the HTML using BeautifulSoup
                     soup = BeautifulSoup(response.content, "html.parser")
 
-                            # find all img tags
+                    # find all img tags
                     image_tags = soup.find_all("img")
 
-                            # download each image and save it to the specified directory
+                    # download each image and save it to the specified directory
                     del image_tags[0]
                     img_data=[]
                     for index,image_tag in enumerate(image_tags):
@@ -59,10 +52,14 @@ def index():
                                 img_data.append(mydict)
                                 with open(os.path.join(save_directory, f"{query}_{image_tags.index(image_tag)}.jpg"), "wb") as f:
                                     f.write(image_data)
-                    client = pymongo.MongoClient("mongodb+srv://priyankanesargi:******@priyankanesargi.cnk5ej5.mongodb.net/?retryWrites=true&w=majority&appName=priyankanesargi")
+                    # if not required to upload in mongodb then can comment below code
+                    #mongodb code starts
+                    mongodb_uri = "mongodb+srv://priyankanesargi:******@priyankanesargi.cnk5ej5.mongodb.net/?retryWrites=true&w=majority&appName=priyankanesargi"
+                    client = pymongo.MongoClient(mongodb_uri)
                     db = client['image_scrap']
                     review_col = db['image_scrap_data']
-                    review_col.insert_many(img_data)          
+                    review_col.insert_many(img_data)  
+                    # mongodb code ends        
 
                     return "image loaded"
                 except Exception as e:
